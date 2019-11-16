bdae47776f22d24548f14e86ac1ea1280afdb9db
Raul Fernandes <raulkfernandes@gmail.com>
2019-11-16 15:44:38 -0300

    Creating package summary activivty

---
+++ app/src/main/java/br/com/alura/easytravel/ui/activity/PackageSummaryActivity.java
@@ -0,0 +1,16 @@
+package br.com.alura.easytravel.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.alura.easytravel.R;
+
+public class PackageSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_summary);
+    }
+}
